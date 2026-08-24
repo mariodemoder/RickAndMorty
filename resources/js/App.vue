938506b0159d47cd6850689b1d92dad50acb032b
Mario Ajenandro Muñoz <maralejandromunoz@gmail.com>
2026-08-24 15:30:00 +0200

    feat: front - openAPI link

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -10,6 +10,16 @@ import Navbar from './components/Navbar.vue';
         </main>
         <footer class="bg-gray-900 border-t border-gray-800 py-6 text-center text-gray-500 text-sm">
             <p>Rick & Morty Explorer &mdash; Built with Laravel + Vue.js</p>
+            <p class="mt-2">
+                <a 
+                    href="http://localhost:8080/api/documentation" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    class="text-gray-400 hover:text-white transition-colors underline"
+                >
+                    Documentación de la API
+                </a>
+            </p>
         </footer>
     </div>
 </template>
